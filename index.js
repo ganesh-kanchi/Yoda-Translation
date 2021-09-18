96c860d89e0d1aa7a86a17d49721e231a7444529
Ganeshkanchimad <86827594+Ganeshkanchimad@users.noreply.github.com>
2021-09-19 00:32:06 +0530

    final commit

---
+++ index.js
@@ -0,0 +1,30 @@
+
+var btnTranslate = document.querySelector("#btn-translate");
+var txtInput = document.querySelector("#txt-input")
+var divOutput = document.querySelector("#div-output")
+
+
+
+var url = "https://api.funtranslations.com/translate/yoda.json"
+
+btnTranslate.addEventListener("click", clickHandler);
+
+
+
+function clickHandler(event) {
+    console.log("clicked");
+    var input = txtInput.value;
+    var finalURL = constructURL(input);
+    console.log(finalURL);
+    fetch(finalURL)
+        .then(response => response.json())
+        .then(json => {
+            divOutput.innerText = json.contents.translated;
+        })
+        .catch(() => alert("some error has occured!"))
+};
+
+function constructURL(Input) {
+    var encodedURI = encodeURI(Input);
+    return `${url}?text=${encodedURI}`;
+}
